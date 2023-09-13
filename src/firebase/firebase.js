9946fe3b16ceb499016538701f8dff66108b6a67
Jonathan Robles <111263727+jonathanrobles@users.noreply.github.com>
2023-09-13 09:00:33 +0800

    update firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCL7ZiNt0QqE2fffHddr63prP-iWuhjf_Q",
-  authDomain: "my-library-f538d.firebaseapp.com",
-  projectId: "my-library-f538d",
-  storageBucket: "my-library-f538d.appspot.com",
-  messagingSenderId: "360172044216",
-  appId: "1:360172044216:web:84ac70feed6f85c9da194d"
+  apiKey: "AIzaSyCYiYLoj1R6SvCfspf2amM5Ng_1PPOQjX8",
+  authDomain: "robles-portfolio.firebaseapp.com",
+  projectId: "robles-portfolio",
+  storageBucket: "robles-portfolio.appspot.com",
+  messagingSenderId: "619730414268",
+  appId: "1:619730414268:web:b11ef79007ee73effafeb9"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore
+const db = getFirestore(app); 
+
+export { db };
